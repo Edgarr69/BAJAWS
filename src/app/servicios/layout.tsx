3655d1da73853dc46a5afecac9a64f7dbd4b944c
Edgar Cabrera <edgar.go2003@gmail.com>
2026-03-27 09:54:52 -0700

    Servicios: agrega Footer al layout

--- src/app/servicios/layout.tsx
+++ src/app/servicios/layout.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function ServiciosLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 }
